03013aadcee9a348ec101bacde65135372467e01
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-23 10:42:44 -0700

    add form

---
+++ app.js
@@ -0,0 +1,31 @@
+const _ = require('lodash')
+
+
+let classArray = ['adonay', 'aldo', 'justin', 'kat', 'kevin', 'kyle', 'lena', 'matt', 'melinda', 'nathan', 'rj', 'ron', 'sean', 'stan']
+
+
+
+function fourGroups(array) {
+  for (let i = 0; i < array.length; i++) {
+
+  }
+}
+
+let groupOne = 'nothing'
+
+console.log(groupOne)
+// console.log(groupTwo)
+// console.log(groupThree)
+// console.log(groupFour)
+
+
+console.log(_.now())
+
+let groupTwo = []
+let groupThree = []
+let groupFour = []
+
+//
+// _.sampleSize(classArray, 4)
+// _.sampleSize(classArray, 3)
+// _.sampleSize(classArray, 3)
